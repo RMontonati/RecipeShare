c8b8217974659ca79d5befe7b5a7dd5e161da19f
Riccardo <riccardo.montonati.rm@gmail.com>
2022-06-22 17:44:54 +0200

    First commit prova

---
+++ app/src/main/java/com/example/recipeshare/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.recipeshare
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn_start = findViewById<Button>(R.id.btn_start)
+        btn_start.setOnClickListener{
+            val et_name = findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.et_name)
+            if (et_name.text.toString().isEmpty()){
+                Toast.makeText(this, "Inserisci il tuo nome", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
